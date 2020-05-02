97151571e27df125e6af09c950f5f8cfd260650a
Mao Contreras <64452973+Maocontreras@users.noreply.github.com>
2020-05-02 08:19:22 -0500

    Create correo.php

---
+++ correo.php
@@ -0,0 +1,8 @@
+<?php 
+	$name = $_POST['name'];
+  $email = $_POST['email'];
+	$subjet = $_POST['subject'];
+	$message = $_POST['message'];
+
+	mail('mauriciocontrerasmantilla@gmail.com', $name, $email, $subjet, $message);
+ ?>
